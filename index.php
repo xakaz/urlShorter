9a2399ebc17e430b0f50235338c7276b7742082c
xakaz <72222889+xakaz@users.noreply.github.com>
2023-02-09 11:20:49 +0100

    bg pix & logo + css

--- index.php
+++ index.php
@@ -3,11 +3,20 @@
 
 <head>
   <meta charset="UTF-8">
-  <link rel="icon" type="image/x-icon" href="./pix//favicon.ico">
+  <link rel="icon" type="image/x-icon" href="./pix/favicon.ico">
+  <link rel="stylesheet" href="style.css">
   <title>Url Shorter</title>
 </head>
 
 <body>
+  <section id="hello">
+    <div class="container">
+      <header>
+        <img src="pix/URL SHORTER.png" alt="logo url" id="logo">
+      </header>
+    </div>
+
+  </section>
 
 </body>
 
