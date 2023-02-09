09853c3be758e9e3503d2332fa4f853f710bafeb
xakaz <72222889+xakaz@users.noreply.github.com>
2023-02-09 11:04:08 +0100

    add css file & pix folder

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" type="image/x-icon" href="./pix//favicon.ico">
+  <title>Url Shorter</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
